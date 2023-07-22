545b595f21d4255c97f1bd3c5c8a1620c4458068
sousuke-m <136174237+sousuke-m@users.noreply.github.com>
2023-07-22 23:19:30 +0900

    lnitial commit

--- Summer_Project/2023/news.js
+++ Summer_Project/2023/news.js
@@ -1,14 +1,14 @@
 function reload_(news_data){
     document.querySelector("body > main > div > ul").innerHTML = '<li>問題なし</li>';
-    for(let i = 0;i < news_data.length;i++){
+    for(let i = 0;i < news_data.news_data.length;i++){
         console.log(document.querySelector("body > main > div > ul"));
-        console.log(`<li>${news_data[i].title}</li>`);
-        document.querySelector("body > main > div > ul").innerHTML += `<li onclick="news_v(${i});">${news_data[i].title}</li>`;
+        console.log(`<li>${news_data.news_data[i].title}</li>`);
+        document.querySelector("body > main > div > ul").innerHTML += `<li onclick="news_v(${i});">${news_data.news_data[i].title}</li>`;
     };
 };
 function news_v(n){
-    document.querySelector("body > div").innerHTML = `<button onclick="news_n();">閉じる</button><h1>${news_data[n].title}</h1>
-<small>${news_data[n].day}</small><br>${news_data[n].text_}`;
+    document.querySelector("body > div").innerHTML = `<button onclick="news_n();">閉じる</button><h1>${news_data.news_data[n].title}</h1>
+<small>${news_data.news_data[n].day}</small><br>${news_data.news_data[n].text_}`;
     document.querySelector("body > div").classList.remove("hide");
 };
 function news_n(){
