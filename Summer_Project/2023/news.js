798d5737b1a2b2ee109a3128c34eb3fc6337558e
sousuke-m <136174237+sousuke-m@users.noreply.github.com>
2023-07-22 22:33:52 +0900

    lnitial commit

--- Summer_Project/2023/news.js
+++ Summer_Project/2023/news.js
@@ -1,17 +1,17 @@
 function reload_(news_data){
     for(let i = 0;i < news_data.length;i++){
-        console.log(document.querySelector("body > div > ul"));
+        console.log(document.querySelector("body > main > div > ul"));
         console.log(`<li>${news_data[i].title}</li>`);
         document.querySelector("body > main > div > ul").innerHTML += `<li onclick="news_v(${i});">${news_data[i].title}</li>`;
     };
 };
 function news_v(n){
-    document.querySelector("body > div").innerHTML = `<button onclick="news_n();">閉じる</button><h1>${news_data[n].title}</h1>
+    document.querySelector(".news_").innerHTML = `<button onclick="news_n();">閉じる</button><h1>${news_data[n].title}</h1>
 <small>${news_data[n].day}</small><br>${news_data[n].text_}`;
-    document.querySelector("body > div").classList.remove("hide");
+    document.querySelector(".news_").classList.remove("hide");
 };
 function news_n(){
-    document.querySelector("body > div").classList.add("hide");
+    document.querySelector(".news_").classList.add("hide");
 };
 fetch('news_data.json')
     .then((data) => data.json())
